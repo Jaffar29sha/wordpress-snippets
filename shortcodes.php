e8ba4593dcd7bbd0d32098fcde186354d074ad9e
Jaffar Sadhik Ibrahimsha <sha16jaffar@gmail.com>
2019-03-23 17:42:15 +0530

    Update shortcodes.php

--- shortcodes.php
+++ shortcodes.php
@@ -1,17 +1,3 @@
-/* HIDE OTHER SHIPPING METHODS IF FREE SHIPPING AVAILABLE */
-
-add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
-function my_hide_shipping_when_free_is_available( $rates ) {
-	$free = array();
-	foreach ( $rates as $rate_id => $rate ) {
-		if ( 'free_shipping' === $rate->method_id ) {
-			$free[ $rate_id ] = $rate;
-			break;
-		}
-	}
-	return ! empty( $free ) ? $free : $rates;
-}
-
 /* CHANGE ADD TO CART TEXT IN SINGLE PRODUCT PAGE */
 
 add_filter('woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text');  
